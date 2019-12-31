703a1de87f9ecb34631a14a29bf27e2fcb70812f
07708@momoshop.com.tw <07708@momoshop.com.tw>
2019-12-31 05:42:31 +0000

    after fixed qryStr

--- unboxing.py
+++ unboxing.py
@@ -73,132 +73,11 @@ SELECT
 FROM t
 """
 
-qryStr0 = """
--- 取得取得資料的時間區段
-DECLARE start_dtraceback_daynum INT64 DEFAULT -3;
-DECLARE end_traceback_daynum INT64 DEFAULT -2;
-DECLARE start_datetime DATETIME DEFAULT DATETIME(TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL start_dtraceback_daynum DAY), "Asia/Taipei");
-DECLARE end_datetime DATETIME DEFAULT DATETIME(TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL end_traceback_daynum DAY), "Asia/Taipei");
 
-SELECT STRING(TIMESTAMP(start_datetime)) as start_datetime,STRING(TIMESTAMP(end_datetime)) as end_datetime
-"""
+# In[21]:
 
 
-# -- 訂單數量
-qryStr1 = """
--- 訂單數量
-DECLARE start_dtraceback_daynum INT64 DEFAULT -3;
-DECLARE end_traceback_daynum INT64 DEFAULT -2;
-DECLARE start_datetime DATETIME DEFAULT DATETIME(TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL start_dtraceback_daynum DAY), "Asia/Taipei");
-DECLARE end_datetime DATETIME DEFAULT DATETIME(TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL end_traceback_daynum DAY), "Asia/Taipei");
-
-SELECT COUNT(DISTINCT orderNo) AS order_count
-FROM `momo-develop.boxSaver.regularQC_slipInfo`
-WHERE DATETIME(orderDate)
-BETWEEN start_datetime AND end_datetime
-"""
-
-# -- 出貨數量
-qryStr2 = """
--- 出貨數量 
-DECLARE start_dtraceback_daynum INT64 DEFAULT -3;
-DECLARE end_traceback_daynum INT64 DEFAULT -2;
-DECLARE start_datetime DATETIME DEFAULT DATETIME(TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL start_dtraceback_daynum DAY), "Asia/Taipei");
-DECLARE end_datetime DATETIME DEFAULT DATETIME(TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL end_traceback_daynum DAY), "Asia/Taipei");
-
-SELECT COUNT(DISTINCT slipNo) AS slip_count
-FROM `momo-develop.boxSaver.regularQC_slipInfo`
-WHERE DATETIME(orderDate)
-BETWEEN start_datetime AND end_datetime 
-"""
-
-# -- 箱出訂單數
-qryStr3 = """
-DECLARE start_dtraceback_daynum INT64 DEFAULT -3;
-DECLARE end_traceback_daynum INT64 DEFAULT -2;
-DECLARE start_datetime DATETIME DEFAULT DATETIME(TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL start_dtraceback_daynum DAY), "Asia/Taipei");
-DECLARE end_datetime DATETIME DEFAULT DATETIME(TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL end_traceback_daynum DAY), "Asia/Taipei");
-
-SELECT COUNT(DISTINCT orderNo) AS isBoxOut_count
-FROM `momo-develop.boxSaver.regularQC_slipInfo`
-WHERE isBoxOut = 1
-AND DATETIME(orderDate)
-BETWEEN start_datetime AND end_datetime 
-"""
-
-qryStr0123 = """
-DECLARE start_dtraceback_daynum INT64 DEFAULT -3;
-DECLARE end_traceback_daynum INT64 DEFAULT -2;
-DECLARE start_datetime DATETIME DEFAULT DATETIME(TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL start_dtraceback_daynum DAY), "Asia/Taipei");
-DECLARE end_datetime DATETIME DEFAULT DATETIME(TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL end_traceback_daynum DAY), "Asia/Taipei");
-
-SELECT 
-    STRING(TIMESTAMP(start_datetime)) as start_datetime,
-    STRING(TIMESTAMP(end_datetime)) as end_datetime,
-    COUNT(DISTINCT orderNo) AS order_count,
-    COUNT(DISTINCT slipNo) AS slip_count,
-    COUNT(DISTINCT IF(isBoxOut = 1, orderNo, NULL)) AS isBoxOut_count
-FROM `momo-develop.boxSaver.regularQC_slipInfo`
-WHERE DATETIME(orderDate)
-BETWEEN start_datetime AND end_datetime
-"""
-# -- 拆箱訂單數(排除箱出) 
-qryStr4 = """
-DECLARE start_dtraceback_daynum INT64 DEFAULT -3;
-DECLARE end_traceback_daynum INT64 DEFAULT -2;
-DECLARE start_datetime DATETIME DEFAULT DATETIME(TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL start_dtraceback_daynum DAY), "Asia/Taipei");
-DECLARE end_datetime DATETIME DEFAULT DATETIME(TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL end_traceback_daynum DAY), "Asia/Taipei");
-
-with a as
-(
-SELECT *
-FROM `momo-develop.boxSaver.regularQC_slipInfo`
-WHERE isboxout<>1
-AND DATETIME(orderDate)
-BETWEEN start_datetime AND end_datetime 
-),
--- 將slipno>1找出來就是拆箱
-b as
-(
-SELECT orderno,count(distinct slipno) as slip_count
-FROM a
-GROUP BY orderno 
-HAVING slip_count > 1
-)
-
--- 印出拆箱訂單數(排除箱出)
-SELECT count(orderno) as split_order_count
-FROM b
-"""
-
-# 拆箱訂單總出貨數
-qryStr5 = """
-DECLARE start_dtraceback_daynum INT64 DEFAULT -3;
-DECLARE end_traceback_daynum INT64 DEFAULT -2;
-DECLARE start_datetime DATETIME DEFAULT DATETIME(TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL start_dtraceback_daynum DAY), "Asia/Taipei");
-DECLARE end_datetime DATETIME DEFAULT DATETIME(TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL end_traceback_daynum DAY), "Asia/Taipei");
-
-with a as
-(
-SELECT *
-FROM `momo-develop.boxSaver.regularQC_slipInfo`
-WHERE isboxout<>1
-AND DATETIME(orderDate)
-BETWEEN start_datetime AND end_datetime 
-),
--- 將slipno>1找出來就是拆箱
-b as
-(
-SELECT orderno,count(distinct slipno) as slip_count
-FROM a
-GROUP BY orderno 
-HAVING slip_count > 1
-)
-
--- 拆箱訂單總出貨數
-SELECT sum(slip_count) as split_slip_count
-FROM b
-"""
+get_ipython().system('touch unboxing.py')
 
 
 # In[19]:
@@ -208,7 +87,7 @@ def getSplitBox(qryStr):
     
     # ref = db.reference('/'+col)
     
-    bq_client = bigquery.Client()
+    bq_client = bigquaery.Client()
     query_job = bq_client.query(qryStr) # API request
 
     rows_df = query_job.result().to_dataframe() # Waits for query to finish
