0a18cd377dfe0478b2fbd5df47b2165854095b1c
07708@momoshop.com.tw <07708@momoshop.com.tw>
2020-01-07 05:45:28 +0000

    測試bigquery取得後檔案格式

---
+++ dd_split.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 套件載入
+from google.cloud import bigquery
+from google.cloud import firestore
+import pandas
+from datetime import datetime
+import pytz
+
+
+# Query Box
+qryStrAll = """
+SELECT * FROM `momo-develop.scheduledReport.dd_split_firestore`
+"""
+
+# In[64]:
+
+
+def get_dd_split(self):
+    # 定義等待容器
+    # box = {}
+    # 定義 document
+    doc = datetime.now(pytz.timezone('Asia/Taipei')).strftime("%Y-%m-%d-%H-%M-%S")
+    # 定義 DB
+    db = firestore.Client()
+    doc_ref = db.collection(u'dd_split').document(doc)
+        
+    bq_client = bigquery.Client()
+    query_job = bq_client.query(qryStrAll) # API request
+
+    rows_df = query_job.result().to_dataframe() # Waits for query to finish
+    postdata = rows_df.to_dict('index')
+
+    # 寫入 DB    
+    doc_ref.set(postdata[0])
+
+    
+
+
+
